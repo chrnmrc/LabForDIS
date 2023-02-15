e0bc644668db695be67787286600ee6d2c663eac
chrnmrc <chernomorecnastya01@gmail.com>
2023-02-15 17:29:45 +0600

    Change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String ss = "Все вокруг нас - это счастье. счастье бывает мимолетным. счастье приходит и уходит. Все счастливы, когда у них все получается. счастье не в деньгах. Огромное счастье можно обрести в кругу родных.";
+        String pat = "счастье";
+        int indPat;
+        int curInd = 0;
+        int counter = 0;
+        while ((indPat = ss.indexOf(pat,curInd)) != -1) {
+            counter++;
+            curInd = indPat +1;
+        }
+        System.out.printf("%s - %d", pat, counter);
     }
 }
